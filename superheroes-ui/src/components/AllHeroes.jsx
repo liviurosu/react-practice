d14fb21742022e27b3ed7ade78f03f0ea578e06b
Liviu <liviurosu08@gmail.com>
2023-08-01 11:24:19 +0100

    ql 7 almost done

--- superheroes-ui/src/components/AllHeroes.jsx
+++ superheroes-ui/src/components/AllHeroes.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import HeroDetail from './HeroDetail'
 import Row from 'react-bootstrap/Row'
+import { useState } from 'react'
+
 
 const AllHeroes = () => {
-    const heros = [
+    const [total, setTotal] = useState(0);
+
+    const totalCountHandler = (name) =>{
+        console.log(name)
+        setTotal(total+1);
+    }
+
+
+    const heroes = [
         { "id": 1, "name": "Bruce Wayne", "alias": "Batman", "superpower": "Martial Arts", "teamID": 1 },
         { "id": 2, "name": "Clark Kent", "alias": "Superman", "superpower": "Flight", "teamID": 1 },
         { "id": 3, "name": "Jay Garrick", "alias": "The Flash", "superpower": "Super-speed", "teamID": 2 },
@@ -17,16 +27,17 @@ const AllHeroes = () => {
     ]
 
     return (
-        heros.map(hero => (
-            <Row >
-                {heros.map(hero => (
-                    <div className='container' key={hero.id}>
-                        <HeroDetail info={hero} />
-                    </div>
-                ))}
-            </Row>
-        ))
-    )
+        <>
+          <h1> Total count is: {total}</h1>
+          <Row >
+          {heroes.map(hero => (
+            <div className='container' key={hero.id.toString()}>
+              <HeroDetail info={hero} parentCount={totalCountHandler}/>
+            </div>
+           ))}
+          </Row>
+        </>
+      )
 }
 
 export default AllHeroes
--- superheroes-ui/src/components/HeroForm.jsx
+++ superheroes-ui/src/components/HeroForm.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Row from 'react-bootstrap/Row';
+import  Button  from 'react-bootstrap/Button';
 
 const HeroForm = () => {
-  return (
-    <div>HeroForm</div>
-  )
+    return (
+        <Row className='heroForm'>
+            <Form >
+                <Form.Group className="mb-3" controlId="alias">
+                    <Form.Label>Hero Alias: </Form.Label>
+                    <Form.Control type="text" placeholder="Hero alias" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="name">
+                    <Form.Label>Hero Name: </Form.Label>
+                    <Form.Control type="text" placeholder="Hero name" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="ability">
+                    <Form.Label>Hero Ability: </Form.Label>
+                    <Form.Control type="text" placeholder="Hero ability" />
+                </Form.Group>
+
+                <Form.Select aria-label="Team ID">
+                <option>Team ID</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+                </Form.Select>
+
+                 <Button variant="primary" type="submit">
+                     Submit
+                 </Button>
+                </Form>
+
+          
+            
+        </Row>
+    )
 }
 
 export default HeroForm
